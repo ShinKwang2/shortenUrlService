c6531bc49f22b868db6661ebe88a4b6fb62bd9d8
ShinKwang2 <shinkwang@yonsei.ac.kr>
2026-03-15 12:06:48 +0900

    init: ShortenUrl Project

---
+++ src/main/java/kr/co/shortenurlservice/application/SimpleShortenUrlService.java
@@ -0,0 +1,83 @@
+package kr.co.shortenurlservice.application;
+
+import kr.co.shortenurlservice.domain.LackOfShortenUrlKeyException;
+import kr.co.shortenurlservice.domain.NotFoundShortenUrlException;
+import kr.co.shortenurlservice.domain.ShortenUrl;
+import kr.co.shortenurlservice.domain.ShortenUrlRepository;
+import kr.co.shortenurlservice.presentation.ShortenUrlCreateRequestDto;
+import kr.co.shortenurlservice.presentation.ShortenUrlCreateResponseDto;
+import kr.co.shortenurlservice.presentation.ShortenUrlInformationDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SimpleShortenUrlService {
+
+    private ShortenUrlRepository shortenUrlRepository;
+
+    @Autowired
+    public SimpleShortenUrlService(ShortenUrlRepository shortenUrlRepository) {
+        this.shortenUrlRepository = shortenUrlRepository;
+    }
+
+    public ShortenUrlCreateResponseDto generateShortenUrl(
+            ShortenUrlCreateRequestDto shortenUrlCreateRequestDto
+    ) {
+        // 1. 단축 URL 키(key) 생성
+        // 2. 원래의 URL과 단축 키를 통해 ShortenUrl 도메인 객체 생성
+        // 3. 생성된 ShortenUrl을 레포지토리를 통해 저장
+        // 4. ShortenUrl을 ShortenUrlCreateResponseDto로 변환하여 반환
+
+        String originalUrl = shortenUrlCreateRequestDto.getOriginalUrl();
+        String shortenUrlKey = ShortenUrl.generateShortenUrlKey();
+
+        ShortenUrl shortenUrl = new ShortenUrl(originalUrl, shortenUrlKey);
+        shortenUrlRepository.saveShortenUrl(shortenUrl);
+
+        ShortenUrlCreateResponseDto shortenUrlCreateResponseDto
+                = new ShortenUrlCreateResponseDto(shortenUrl);
+
+        return shortenUrlCreateResponseDto;
+    }
+
+    public String getOriginalUrlByShortenUrlKey(String shortenUrlKey) {
+        ShortenUrl shortenUrl = shortenUrlRepository.findShortenUrlByShortenUrlKey(shortenUrlKey);
+
+        if (null == shortenUrl) {
+            throw new NotFoundShortenUrlException();
+        }
+
+        shortenUrl.increaseRedirectCount();
+        shortenUrlRepository.saveShortenUrl(shortenUrl);
+
+        String originalUrl = shortenUrl.getOriginalUrl();
+
+        return originalUrl;
+    }
+
+    public ShortenUrlInformationDto getShortenUrlInformationByShortenUrlKey(String shortenUrlKey) {
+        ShortenUrl shortenUrl = shortenUrlRepository.findShortenUrlByShortenUrlKey(shortenUrlKey);
+
+        if (null == shortenUrl) {
+            throw new NotFoundShortenUrlException();
+        }
+
+        ShortenUrlInformationDto shortenUrlInformationDto = new ShortenUrlInformationDto(shortenUrl);
+        return shortenUrlInformationDto;
+    }
+
+    private String getUniqueShortenUrlKey() {
+        final int MAX_RETRY_COUNT = 5;
+        int count = 0;
+        while (count++ < MAX_RETRY_COUNT) {
+            String shortenUrlKey = ShortenUrl.generateShortenUrlKey();
+            ShortenUrl shortenUrl = shortenUrlRepository.findShortenUrlByShortenUrlKey(shortenUrlKey);
+
+            if (null == shortenUrl) {
+                return shortenUrlKey;
+            }
+        }
+
+        throw new LackOfShortenUrlKeyException();
+    }
+}
---
+++ src/main/java/kr/co/shortenurlservice/domain/ShortenUrlRepository.java
@@ -0,0 +1,10 @@
+package kr.co.shortenurlservice.domain;
+
+import org.springframework.stereotype.Repository;
+
+public interface ShortenUrlRepository {
+
+    void saveShortenUrl(ShortenUrl shortenUrl);
+
+    ShortenUrl findShortenUrlByShortenUrlKey(String shortenUrlKey);
+}
